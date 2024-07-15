c731ea18576712bbf07a5887e5480befdda5c293
Durgesh kushwaha <durgeshkushwaha415@gamil.com>
2024-07-15 18:10:15 +0530

    completed

--- controllers/hisaab-controller.js
+++ controllers/hisaab-controller.js
@@ -1,35 +1,119 @@
+const { isLoggendIn } = require("../middlewares/auth-middlewares");
 const hisaabModel = require("../models/hisab-model");
 const userModel = require("../models/user-model");
 
-module.exports.createHisaabController = async function(req,res){
-let{title,
-description,
-encrypted,
-shareable,
-passcode,
-editpermissions} = req.body;
+module.exports.createHisaabController = async function (req, res) {
+  let { title, description, encrypted, shareable, passcode, editpermissions } =
+    req.body;
 
-encrypted = encrypted === "on" ? true :false;
-shareable = shareable === "on" ? true :false;
-editpermissions= editpermissions=== "on" ? true :false;
+  encrypted = encrypted === "on" ? true : false;
+  shareable = shareable === "on" ? true : false;
+  editpermissions = editpermissions === "on" ? true : false;
 
-
-let hisaabcreated = await hisaabModel.create({
+  let hisaabcreated = await hisaabModel.create({
     title,
     description,
-    user:req.user._id,
+    user: req.user._id,
     passcode,
     encrypted,
     shareable,
-    editpermissions
-});
-let user = await userModel.findOne({email:req.user.email});
-user.hisaab.push(hisaabcreated._id);
-await user.save();
+    editpermissions,
+  });
+  let user = await userModel.findOne({ email: req.user.email });
+  user.hisaab.push(hisaabcreated._id);
+  await user.save();
 
-res.redirect("/profile")
+  res.redirect("/profile");
 };
 
-module.exports.hisaabPageController = async function(req,res){
-        res.render("create");
+module.exports.hisaabPageController = async function (req, res) {
+  res.render("create");
 };
+
+
+module.exports.readHisaabController = async function (req, res, next) {
+    try {
+        const id = req.params.id;
+        const hisaab = await hisaabModel.findOne({
+            _id: id,
+        });
+        if (!hisaab) {
+            return res.redirect("/profile");
+        }
+        if (hisaab.encrypted) {
+            return res.render("passcode", { isLoggedIn: true, id });
+        }
+        res.render("hisaab", { isLoggedIn: true, hisaab });
+    } catch (error) {
+        next(error); // Pass errors to Express error handler
+    }
+};
+
+
+module.exports.readVerifiedHisaabController = async function (req, res, next) {
+  try {
+      const id = req.params.id;
+      const hisaab = await hisaabModel.findOne({ _id: id });
+      if (!hisaab) {
+          return res.redirect('/profile');
+      }
+      if (hisaab.passcode !== req.body.passcode) {
+          return res.redirect('/profile');
+      }
+      return res.render("hisaab", {
+          isLoggedIn: true,
+          hisaab
+      });
+  } catch (error) {
+      next(error); // Pass errors to Express error handler
+  }
+};
+
+module.exports.deletecontroller = async function (req, res, next) {
+  try {
+      const id = req.params.id;
+      const hisaab = await hisaabModel.findOne({
+          _id: id,
+          user: req.user.id
+      });
+
+      if (!hisaab) {
+          return res.redirect("/profile");
+      }
+
+      await hisaabModel.deleteOne({ _id: id });
+      return res.redirect("/profile");
+  } catch (error) {
+      next(error); // Pass errors to Express error handler
+  }
+};
+
+
+module.exports.editcontroller = async function(req,res,next){
+  const id = req.params.id;
+  const hisaab = await hisaabModel.findById(id);
+
+  if(!hisaab){
+    return res.redirect("/profile")
+  }
+  return res.render("edit",{isLoggendIn:true,hisaab})
+}
+
+module.exports.editpostcontroller = async function(req,res,next){
+  const id = req.params.id;
+  const hisaab = await hisaabModel.findById(id);
+
+  if(!hisaab){
+    return res.redirect("/profile")
+  }
+  hisaab.title = req.body.title;
+  hisaab.description = req.body.description;
+  hisaab.editpermissions = req.body.editpermissions == 'on'? true:false;
+  hisaab. encrypted = req.body. encrypted == 'on'? true:false;
+  hisaab.passcode = req.body.passcode;
+  hisaab.shareable = req.body.shareable == 'on'? true:false;
+
+  await hisaab.save();
+  res.redirect("/profile")
+
+}
--- routes/hisaab-router.js
+++ routes/hisaab-router.js
@@ -4,13 +4,29 @@ const router = express.Router();
 const {
   isLoggendIn,
   redirectIfLoggedIn,
+
 } = require("../middlewares/auth-middlewares");
 const {
   createHisaabController,
   hisaabPageController,
+  readHisaabController,
+  deletecontroller,
+  editcontroller,
+  editpostcontroller,
+  readVerifiedHisaabController
 } = require("../controllers/hisaab-controller");
+
+
+router.post("/:id/verify", isLoggendIn, readVerifiedHisaabController);
 
 router.get("/create", isLoggendIn, hisaabPageController);
 router.post("/create", isLoggendIn, createHisaabController);
+
+router.get("/delete/:id",isLoggendIn,deletecontroller)
+router.get("/edit/:id",isLoggendIn,editcontroller)
+router.post("/edit/:id",isLoggendIn,editpostcontroller)
+
+
+router.get("/view/:id",isLoggendIn, readHisaabController);
 
 module.exports = router;
